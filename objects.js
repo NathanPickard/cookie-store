0c2ef7e8da3e90ae9b903be19d796e377adb89ce
Nathan Pickard <nathan.pickard87@gmail.com>
2016-07-16 10:15:17 -0700

    Added index.html and objects.js

---
+++ objects.js
@@ -0,0 +1,39 @@
+var pioneerSquare = {
+  name: "Pioneer Square",
+  customerMin: 17,
+  customerMax: 88,
+  customerCookieAvg: 5.2
+  generateRandom: function() {
+    var list = documents.getElementById("")
+  }
+}
+
+var portlandAirport = {
+  name: "Portland Airport",
+  customerMin: 17,
+  customerMax: 88,
+  customerCookieAvg: 5.2
+  generateRandom: function() {
+    var list = documents.getElementById("")
+  }
+}
+
+var pioneerSquare = {
+  name: "Pioneer Square",
+  customerMin: 17,
+  customerMax: 88,
+  customerCookieAvg: 5.2
+  generateRandom: function() {
+    var list = documents.getElementById("")
+  }
+}
+
+var pioneerSquare = {
+  name: "Pioneer Square",
+  customerMin: 17,
+  customerMax: 88,
+  customerCookieAvg: 5.2
+  generateRandom: function() {
+    var list = documents.getElementById("")
+  }
+}
